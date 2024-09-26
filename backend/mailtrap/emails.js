2d10bec99b35ed6a14046b8948d7984a265a7e3a
Kidus Petros <kidusptr@gmail.com>
2024-09-26 14:28:53 +0300

    implemented verification email after signup

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -21,3 +21,20 @@ export const sendVerificationEmail = async (email, verificationCode) => {
     throw error;
   }
 };
+
+export const welcomeEmail = async (email) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to our platform",
+      html: "<h1>Welcome to our platform</h1>",
+      category: "Welcome Email",
+    });
+    console.log("Email sent:", response);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
